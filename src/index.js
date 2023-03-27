7b952e71c27a8062df95e263c02d6d82134f2a71
Lucie Caillat <lucie.caillat@gmail.com>
2023-03-27 10:52:43 +0200

    add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link} from 'react-router-dom';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Error from './pages/Error';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <React.StrictMode>
-  <Home/>
-  <AboutUs/>
-  <Error/>
+  <Router>
+    <div>
+      <nav>
+        <Link to="/">Accueil</Link>
+        <Link to="about">À propos</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="about" element={<AboutUs/>}/>
+        <Route path=":error" element={<Error/>}/>
+      </Routes>
+
+    </div>
+  </Router>
 </React.StrictMode>  
 );
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 function Home(){
-  return <h1>Page d'acceil</h1>
+  return <h1>Page d'accueil</h1>
 }
 
 export default Home
