e8f9ae51ca035ad652f15f2283061544702bb247
Lucie Caillat <lucie.caillat@gmail.com>
2023-03-28 17:41:03 +0200

    fomatting error

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,3 +1,10 @@
+import { Link} from 'react-router-dom';
+import './error.css'
+
 export default function Error(){
-  return <h1>Erreur 404</h1>
+  return <div className='error-box'>
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className='link'>Retourner sur la page d’accueil</Link>
+    </div>
 }
