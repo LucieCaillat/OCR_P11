176dc62776bdc370a344b025620aafc1b904e059
Lucie Caillat <lucie.caillat@gmail.com>
2023-03-29 19:59:59 +0200

    hidden when is just on pictue

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -5,6 +5,7 @@ import './gallery.css'
 export default function Gallery({pictures, title}){
   const[imageIndex, updateImageIndex] = useState(0)
   const indexOflastPicture = pictures.length - 1
+  const isJustOnePicture = indexOflastPicture === 0
   function handleClickLeft(){
     if(imageIndex === 0){      
       updateImageIndex(indexOflastPicture)
@@ -20,9 +21,9 @@ export default function Gallery({pictures, title}){
     }  
   }
   return <div className='gallery'>
-    <i className="fa-solid fa-chevron-left" onClick={ handleClickLeft}></i>
+    <i className={`fa-solid fa-chevron-left ${isJustOnePicture? 'hidden' : ''}`} onClick={ handleClickLeft}></i>
     <img src={pictures[imageIndex]} alt={title} className="gallery-cover"/>
-    <i className="fa-solid fa-chevron-right" onClick={handleClickRight}></i>
+    <i className={`fa-solid fa-chevron-right ${isJustOnePicture? 'hidden' : ''}`} onClick={handleClickRight}></i>
 
   </div>
 
